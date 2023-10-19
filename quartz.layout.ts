7067b64f5017f59f4ea04a154871e7267a3d4895
Joaquin dela Rosa <62501229+TheRealAnonymous008@users.noreply.github.com>
2023-10-20 07:11:17 +0800

    style: add recent remove breadcrumbs

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -15,7 +15,6 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -31,6 +30,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.RecentNotes()
   ],
 }
 
