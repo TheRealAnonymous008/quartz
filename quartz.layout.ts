c1803918b67cc3a04662709b984f6e9d777de4c0
TheRealAnonymous008 <joaquinantoniodelarosa@gmail.com>
2023-11-29 07:29:33 +0800

    Revert "Update quartz.layout.ts"
    This reverts commit e044a386adf4da7e2401b2e37146989b943a4ab5.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -15,17 +15,18 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.PageTitle(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
-
-  ],
-  right: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
+  ],
+  right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
